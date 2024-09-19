73082af1600bad11639ee55284ca7d703e671b1d
dejv28 <davidkubicek2010@gmail.com>
2024-09-19 13:11:38 +0200

    Add files via upload

---
+++ website.py
@@ -0,0 +1,16 @@
+
+<html>
+    <h1>
+        kdo to cte je kokot
+    </h1>
+    <body>
+        <h2>About me</h2>
+        <p>
+            Nejsem famboy
+        </p>
+        <h2>What I am working on</h2>
+        <p>
+            Momentalne nejsem gay
+        </p>
+    </body>
+</html>
